2b2824748449cb6b552eca22d702b0d784cc2b4e
chanjianliu <62577341+chanjianliu@users.noreply.github.com>
2020-10-04 15:36:04 +0800

    added ProductList Class

---
+++ CA1/Models/ProductList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CA1.Models
+{
+    public class ProductList
+    {
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public string PhotoLink { get; set; }
+
+    }
+}
