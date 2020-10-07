a30ad3691a30e311e1e6e82e8d321da9fd790d1d
Hongduoooo <e0641597@e.nus.edu>
2020-10-07 11:52:02 +0800

    c

---
+++ CA1/Controllers/RegisterController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CA1.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["title"] = "Register";
+            ViewData["Is_Register"] = "menu_hilite";
+            return View();
+        }
+    }
+}
