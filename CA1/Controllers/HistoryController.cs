f2ce8598c4320d7838f245ce545af426a0d200a8
chanjianliu <62577341+chanjianliu@users.noreply.github.com>
2020-10-10 15:08:44 +0800

    Added Checkout in HistoryController, added a new unsuccessful page (should be used with the payment logic), added a checkout JavaScript file that handles the event when user click checkout button.

---
+++ CA1/Controllers/HistoryController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using CA1.Database;
+using CA1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CA1.Controllers
+{
+    public class HistoryController : Controller
+    {
+        private readonly DbGallery db;
+        public HistoryController(DbGallery db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            string sessionId = HttpContext.Request.Cookies["sessionId"];
+            if (sessionId == null)
+            {
+                return Json(new
+                {
+                    status = "redirect",
+                    url = "/Login/Index"
+                });
+            }
+            Session session = db.Sessions.FirstOrDefault(x => x.Id.ToString() == sessionId);
+            List<Order> orders = (List<Order>)db.Orders.Where(x => x.UserId == session.UserId);
+            ViewData["Order"] = orders;  // ==> timestamp, userId
+            //List<string> productIds = new List<string>();
+            //List<string> ActivationCodes = new List<string>();  = (List<OrderDetail>)db.OrderDetails.Where(x => x.OrderId == orders[i].Id);
+            List<OrderDetail> orderDetails = new List<OrderDetail>();
+            for (int i = 0; i < orders.Count(); i++)
+            {
+                orderDetails.Add((OrderDetail)orders[i].OrderDetails);
+            }
+            ViewData["OrderDetails"] = orderDetails;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CheckOut()
+        {
+            string sessionId = HttpContext.Request.Cookies["sessionId"];
+            User user = db.Users.FirstOrDefault(x => x.Id == db.Sessions.FirstOrDefault(y => y.Id.ToString() == sessionId).UserId);
+            List<ShoppingCartDetail> shoppingcart = db.ShoppingCart.Where(c => c.UserId == user.Id).ToList();
+
+            Order newOrder = new Order()
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserId = user.Id,
+                TransactionDate = DateTime.Now
+            };
+
+            db.Orders.Add(newOrder);
+
+            for (int i = 0; i < shoppingcart.Count; i++)
+            {
+                for (int j = 0; j < shoppingcart[i].Quantity; j++)
+                {
+                    db.OrderDetails.Add(new OrderDetail
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        OrderId = newOrder.Id,
+                        ProductId = shoppingcart[i].ProductId,
+                        ActivationCode = Guid.NewGuid()
+                    });
+                }
+            }
+
+            db.SaveChanges();
+
+            return Json(new {
+                status = "success",
+                url = "/Payment/Index"
+            });
+        }
+
+        public IActionResult Unsuccessful()
+        {
+            return View();
+        }
+    }
+}
