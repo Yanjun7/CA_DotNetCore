3fc83f1b8cfc6f150a4fe6468a84b2b229feee51
JerryJoojoo <1365779897@qq.com>
2020-10-11 12:30:29 +0800

    1. Add remove controller. 2. Modify the shoppingcart html, add a divider.

---
+++ CA1/wwwroot/js/product/search.js
@@ -0,0 +1,16 @@
+﻿window.onload = function ()
+{
+	let form = document.getElementById("searchform");
+
+	form.onsubmit = function ()
+	{
+		let searchElem = document.getElementById("search");
+		let searchResult = searchElem.value.trim();
+		if (searchResult.length !== 0)
+		{
+			return true;	
+		}
+		return false;	
+	}
+}
+    
